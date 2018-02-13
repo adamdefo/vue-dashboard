0a511e6eba9d0365e1b48bbeaef5faea67aca7df
Кондратьев Алексей Викторович <Aleksey.Kondratyev@UF.RT.RU>
2018-02-13 13:50:00 +0300

    Get Article

--- src/components/blog/Blog.vue
+++ src/components/blog/Blog.vue
@@ -1,78 +1,69 @@
 <template>
-  <div class="blog">
-    <div class="title">
-      <h1>{{ title }}</h1>
-      <div class="title__links">
-        <button @click="showAddArticleForm">Добавить</button>
-      </div>
-    </div>
-    <div class="blog__article-list">
-      <ul>
-        <li v-for="article in articles">
-          <a href="#">{{ article.title }}</a>
-        </li>
-      </ul>
-    </div>
-    <div v-if="isShowArticleForm" class="blog__form">
-      <form class="form" @submit.prevent="saveArticle" enctype="multipart/form-data">
-        <div class="form__group">
-          <div class="form__group-item">
-            <label class="form__label">Заголовок</label>
-            <input class="form__control" type="text" v-model="article.title" />
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-item">
-            <label class="form__label">Краткое описание</label>
-            <input class="form__control" type="text" v-model="article.meta.description" />
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-item">
-            <label class="form__label">Ключевые слова</label>
-            <input class="form__control" type="text" v-model="article.meta.keywords" />
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-item">
-            <label class="form__label">Содержание</label>
-            <textarea class="form__control form__control_txt" type="text" v-model="article.content"></textarea>
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-item">
-            <label class="form__label">
-              <input class="form__control form__control_checkbox" type="checkbox" v-model="article.isDraft" /><span>черновик</span>
-            </label>
-          </div>
-        </div>
-        <div class="form__group form__group_btn">
-          <button class="btn" type="submit">Сохранить</button>
-        </div>
-      </form>
-    </div>
-  </div>
+<div class="blog">
+	<div class="title">
+		<h1>{{ title }}</h1>
+		<div class="title__links">
+			<button @click="showAddArticleForm">Добавить</button>
+		</div>
+	</div>
+	<div class="blog__article-list">
+		<ul>
+			<li v-for="article in articles">
+				<a @click.prevent="getArticle(article)">{{ article.title }}</a>
+			</li>
+		</ul>
+	</div>
+	<div v-if="isShowArticleForm" class="blog__form">
+		<form class="form" @submit.prevent="saveArticle" enctype="multipart/form-data">
+			<div class="form__group">
+				<div class="form__group-item">
+					<label class="form__label">Заголовок</label>
+					<input class="form__control" type="text" v-model="article.title" />
+				</div>
+			</div>
+			<div class="form__group">
+				<div class="form__group-item">
+					<label class="form__label">Краткое описание</label>
+					<input class="form__control" type="text" v-model="article.meta.description" />
+				</div>
+			</div>
+			<div class="form__group">
+				<div class="form__group-item">
+					<label class="form__label">Ключевые слова</label>
+					<input class="form__control" type="text" v-model="article.meta.keywords" />
+				</div>
+			</div>
+			<div class="form__group">
+				<div class="form__group-item">
+					<label class="form__label">Содержание</label>
+					<textarea class="form__control form__control_txt" type="text" v-model="article.content"></textarea>
+				</div>
+			</div>
+			<div class="form__group">
+				<div class="form__group-item">
+					<label class="form__label">
+						<input class="form__control form__control_checkbox" type="checkbox" v-model="article.isDraft" /><span>черновик</span>
+					</label>
+				</div>
+			</div>
+			<div class="form__group form__group_btn">
+				<button class="btn" type="submit">Сохранить</button>
+			</div>
+		</form>
+	</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Users',
+  name: 'Blog',
   data: function () {
     return {
       api: '../api/',
       url: 'https://jsonplaceholder.typicode.com',
       title: 'Статьи',
       loading: false,
-      articles: [
-        {
-          id: 1,
-          title: 'Article 1'
-        },
-        {
-          id: 2,
-          title: 'Article 2'
-        }
-      ],
+      articles: [],
       articleImage: '', // обложка для статьи
       article: {
         title: '',
@@ -90,15 +81,19 @@ export default {
   methods: {
     getArticles: function () {
       let vm = this
-      this.$http.get(this.api + '?articles').then(function (response) {
-        let articles = response.data
-        vm.articles = articles.slice()
+      this.$http.get(this.api).then(function (response) {
         console.log(response)
+        let articles = response.data
+        vm.articles = articles.list.slice()
         this.loading = true
       }, function (error) {
         console.log(error)
         this.loading = true
       })
+    },
+    getArticle: function (article) {
+      console.log(article)
+      this.isShowArticleForm = true
     },
     saveArticle: function () {
       this.loading = false
@@ -140,7 +135,7 @@ export default {
     }
   },
   created: function () {
-    // this.getArticles()
+    this.getArticles()
   },
   computed: {
     // isValid: function () {
@@ -152,40 +147,40 @@ export default {
 
 <style lang="scss" scoped>
 .blog {
-  &__article-list {
-    // border-left: 1px solid #dedede; 
-    & ul {
-      margin-left: 16px;
-    }
-  }
+	&__article-list {
+		// border-left: 1px solid #dedede; 
+		& ul {
+			margin-left: 16px;
+		}
+	}
 }
-
 
 .title {
-  padding: 2.5rem 0 1.5rem;
-  & > h1 {
-    border-bottom: 1px solid #dedede;
-    margin: 0 0 1.5rem;
-  }
-  &__links {
-    font-size: 1.3rem;
-    & > a {
-      margin-right: 1rem;
-    }
-  }
+	padding: 2.5rem 0 1.5rem;
+	& > h1 {
+		border-bottom: 1px solid #dedede;
+		margin: 0 0 1.5rem;
+	}
+	&__links {
+		font-size: 1.3rem;
+		& > a {
+			margin-right: 1rem;
+		}
+	}
 }
+
 .list {
-  & > li {
-    font-size: 1.5rem;
-    margin: 1.5rem 0;
-  }
-  & b {
-    font-weight: bold;
-  }
+	& > li {
+		font-size: 1.5rem;
+		margin: 1.5rem 0;
+	}
+	& b {
+		font-weight: bold;
+	}
 }
 
 .loading {
-  text-align: center;
-  font-size: 1.3rem;
+	text-align: center;
+	font-size: 1.3rem;
 }
 </style>
